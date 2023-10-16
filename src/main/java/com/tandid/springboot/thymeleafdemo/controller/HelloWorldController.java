8d806eea216971be9eb31084d68b263025a6e8e7
Tandid Alam <tandid.alam@gmail.com>
2023-10-16 18:10:20 -0400

    Added forms and css

---
+++ src/main/java/com/tandid/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.tandid.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    @GetMapping("/processForm")
+    public String processForm(){
+        return "helloworld";
+    }
+
+
+}
